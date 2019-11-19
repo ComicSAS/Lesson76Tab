c3a756a53970812f2df0f9e20b037b33fddc1c3c
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-11-19 22:40:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lesson76tab/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.lesson76tab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabHost tabHost = findViewById(android.R.id.tabhost);
+        // инициализация
+        tabHost.setup();
+        TabSpec tabSpec;
+        // создаем вкладку и указываем тег
+        tabSpec = tabHost.newTabSpec("tag1");
+        // название вкладки
+        tabSpec.setIndicator("Tab 1");
+        // указываем id компонента из FrameLayout, он и станет содержимым
+        tabSpec.setContent(R.id.tvTab1);
+        // добавляем в корневой элемент
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        // указываем название и картинку
+        // в нашем случае вместо картинки идет xml-файл,
+        // который определяет картинку по состоянию вкладки
+        tabSpec.setIndicator("Tab 2", getResources().getDrawable(R.drawable.tab_icon_selector));
+        tabSpec.setContent(R.id.tvTab2);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag3");
+        // создаем View из layout-файла
+        View v = getLayoutInflater().inflate(R.layout.tab_header, null);
+        // и устанавливаем его, как заголовок
+        tabSpec.setIndicator(v);
+        tabSpec.setContent(R.id.tvTab3);
+        tabHost.addTab(tabSpec);
+        // вторая вкладка будет выбрана по умолчанию
+        tabHost.setCurrentTabByTag("tag2");
+
+        // обработчик переключения вкладок
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
+
